0cc8bc8ae6239c25b33c3e5f1500d8f93012ff59
NicolasFrouin <frouinnicolas@gmail.com>
2024-04-26 15:46:53 +0200

    fix win condition

--- src/game/quota.ts
+++ src/game/quota.ts
@@ -1,16 +1,16 @@
 import { initWinScreen } from "./screens";
 
-export async function initQuota() {  
-    if (!WA.state.quota || isNaN(WA.state.total)) {
-        WA.state.saveVariable("quota", 420);
+export async function initQuota() {
+  if (!WA.state.quota || isNaN(WA.state.total)) {
+    WA.state.saveVariable("quota", 420);
+  }
+  console.log(`Current quota : ${WA.state.quota} `);
+  WA.state.onVariableChange("total").subscribe((total) => {
+    if (Number(total) >= Number(WA.state.quota)) {
+      initWinScreen();
     }
-    console.log(`Current quota : ${WA.state.quota} `);
-    WA.state.onVariableChange("total").subscribe(total => {
-        if (total >= WA.state.quota) {
-            initWinScreen();
-        }  
-    })
-    return WA.state.quota;
+  });
+  return WA.state.quota;
 }
 
 export const gameQuota = initQuota();
